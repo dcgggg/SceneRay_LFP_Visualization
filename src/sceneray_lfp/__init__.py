55e56c84ffc5da53405051c5d6e7384352da19db
dcgggg <dcgggg@users.noreply.github.com>
2026-09-06 02:12:55 +0800

    Clear repository contents while preserving history

--- src/sceneray_lfp/__init__.py
+++
@@ -1,6 +0,0 @@
-"""SceneRay LFP analysis package."""
-
-from .pipeline import AnalysisConfig, AnalysisResult, analyze_file
-
-__all__ = ["AnalysisConfig", "AnalysisResult", "analyze_file"]
-__version__ = "0.2.0"
